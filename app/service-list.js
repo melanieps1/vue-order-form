eb38e02ce33b5fb49c74e5e6c787ef4811f7acf2
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-04 15:04:27 -0400

    added service list in separate file

---
+++ app/service-list.js
@@ -0,0 +1,23 @@
+var serviceOptions = [
+
+    {
+      "name": "Web Development",
+      "price": "$300.00"
+    },
+
+    {
+      "name": "Design",
+      "price": "$400.00"
+    },
+
+    {
+      "name": "Integration",
+      "price": "$250.00"
+    },
+
+    {
+      "name": "Training",
+      "price": "$220.00"
+    }
+
+  ]
