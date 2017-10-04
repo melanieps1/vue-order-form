59df2a2bd2180e0919c7e74f008eb151dbaaf191
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-04 16:09:33 -0400

    console.log for getTotal() is working

--- app/main.js
+++ app/main.js
@@ -4,19 +4,27 @@ var demo = new Vue({
 
 	data: {
 		total: 0,
-		serviceOptions: Object.keys(serviceOptions),
+		services: Object.keys(serviceOptions),
 		price: 0
 	},
 
 	methods: {
-		
+
+		// services( function loopOverKeys(key) {
+		// 	console.log("key: ", key);
+		// 	console.log("name: ", name);
+		// 	console.log("price: ", price);
+		// },
+
 		getPrice: function(serviceOptions) {
+			console.log('I hope this is working');
 			price = this.serviceOptions.price;
+			console.log(price);
 		},
 
 		getTotal: function(total, price) {
+			console.log('Click to get total');
 			total += this.price;
-			
 		}
 
 	}
