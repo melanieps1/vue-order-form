b442f4d643c7d37e9413b79c856cad5a78fef0f4
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-05 13:46:45 -0400

    pulled prices and names in, formatted them to be USD.

--- app/main.js
+++ app/main.js
@@ -1,30 +1,42 @@
+// Define a custom filter called "currency".  It will reformat a number to be `$0.00`.
+
+Vue.filter('currency', function(value) {
+	// `toFixed(#)` gives it two decimal points
+	return '$' + value.toFixed(2);
+});
+
 var demo = new Vue({
 
 	el: '#main',
 
 	data: {
-		total: 0,
-		services: Object.keys(serviceOptions),
-		price: 0
+		services: [
+			{
+				name: 'Web Development',
+				price: 300
+			},
+			{
+				name: 'Design',
+				price: 400
+			},
+			{
+				name: 'Integration',
+				price: 250
+			},
+			{
+				name: 'Training',
+				price: 220
+			}
+		]
+
 	},
 
 	methods: {
 
-		// getPrice: function(services) {
-		// 	console.log('Why won\'t you work!?!');
-		// 	price = this.serviceOptions.price
-		// 	console.log(price);
-		// },
+		total: function() {
+			var total = 0;
 
-		// getPrice: function(services) {
-		// 	console.log('Why won\'t you work!?!');
-		// 	price = this.serviceOptions.price;
-		// 	console.log(price);
-		// },
-
-		getTotal: function(total, price) {
-			console.log('Item added to cart');
-			total += this.price;
+			return total;
 		}
 
 	}
